41d3a9d83ee33d045e0ce7816a530684cf29302d
Rithik Suthan S <rithiksuthan.smartail@gmail.com>
2024-01-06 12:22:30 +0530

    updated the environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   version: '0.0.1',         
-  register_service_url:"http://localhost:8080",
+  register_service_url:"https://todoservice-7rzh.onrender.com",
 };
 
 
