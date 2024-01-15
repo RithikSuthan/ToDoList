47051b43924351fe2b31ca31fe21cf834d3a2d9d
Rithik Suthan S <rithiksuthan.smartail@gmail.com>
2024-01-15 18:33:30 +0530

    done with the header

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { TodoserviceService } from 'services/todoservice.service';
 
 @Component({
@@ -8,31 +10,44 @@ import { TodoserviceService } from 'services/todoservice.service';
 })
 export class RegisterComponent implements OnInit {
 
+  viewLogin:any;
   data={
     emailId:'',
     password:''
   }
-  constructor(private service:TodoserviceService) { }
+  constructor(private service:TodoserviceService,private router :Router) { }
 
   ngOnInit(): void {
     localStorage.removeItem("userId");
+    this.viewLogin=true;
   }
-  registerUser()
-  {
+  registerUser() {
     console.log(this.data);
     this.service.addNewUser(this.data).subscribe(
       (response) => {
         console.log('API Response:', response);
         if (response && response.status === 'success') {
           console.log('User added successfully:', response.message);
+          alert('User added successfully');
+          this.router.navigate(['/', 'login'])
         } else {
           console.error('Error:', response.message);
+          alert(response.message);
         }
       },
       (error) => {
         console.error('API Error:', error);
+  
+        if (error instanceof HttpErrorResponse && error.status === 409) {
+          console.log('User already exists:', error.error.message);
+          alert('User already exists: ' + error.error.message);
+        } else {
+          console.error('Unexpected error:', error.message);
+          alert('Unexpected error occurred');
+        }
       }
     );
   }
+  
   
 }
